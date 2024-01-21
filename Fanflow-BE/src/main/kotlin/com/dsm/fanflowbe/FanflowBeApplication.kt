ac89f73c337fb7ad7c85753c9d37518d4455e6f5
soohyeon <meltapplee@gmail.com>
2024-01-22 02:00:30 +0900

    ⚙️ :: 세팅

---
+++ Fanflow-BE/src/main/kotlin/com/dsm/fanflowbe/FanflowBeApplication.kt
@@ -0,0 +1,11 @@
+package com.dsm.fanflowbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FanflowBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<FanflowBeApplication>(*args)
+}
