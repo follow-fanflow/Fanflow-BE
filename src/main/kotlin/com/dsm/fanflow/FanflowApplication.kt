dca37cee32b322770270be60305da9d0250e3cfd
soohyeon <meltapplee@gmail.com>
2024-01-22 21:17:57 +0900

    ⚙️ :: 세팅

---
+++ src/main/kotlin/com/dsm/fanflow/FanflowApplication.kt
@@ -0,0 +1,11 @@
+package com.dsm.fanflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FanflowApplication
+
+fun main(args: Array<String>) {
+	runApplication<FanflowApplication>(*args)
+}
---
+++ src/test/kotlin/com/dsm/fanflow/FanflowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dsm.fanflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FanflowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
