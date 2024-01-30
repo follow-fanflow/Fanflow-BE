33ba6918f93b6da82a0e15386a3fec46f600db15
soohyeon <meltapplee@gmail.com>
2024-01-31 07:37:13 +0900

    modify :: gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.7"
+	id("org.springframework.boot") version "2.7.2"
 	id("io.spring.dependency-management") version "1.0.12.RELEASE"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
 }
 
@@ -26,20 +26,40 @@ repositories {
 }
 
 dependencies {
+	//DB
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("io.jsonwebtoken:jjwt:0.9.0")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	annotationProcessor("org.projectlombok:lombok")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("mysql:mysql-connector-java")
+
+	//Web
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	//Security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	//Kotlin
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	//Test
+	implementation("org.springframework.boot:spring-boot-starter-test:2.7.16")
 	testImplementation("org.springframework.security:spring-security-test")
-	compileOnly("org.projectlombok:lombok")
+
+	//Validation
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	//jwt
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+	//json
+	implementation("org.json:json:20200518")
+
+	//lombok
+	compileOnly ("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+
+
 }
 
 tasks.withType<KotlinCompile> {
