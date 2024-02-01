beceb561b6981d26ad04018c80dd042180a793c2
soohyeon <meltapplee@gmail.com>
2024-02-01 20:47:30 +0900

    modify :: domain

--- src/main/kotlin/com/dsm/fanflow/domain/log/domain/Log.kt
+++ src/main/kotlin/com/dsm/fanflow/domain/log/domain/Log.kt
@@ -1,31 +1,34 @@
 package com.dsm.fanflow.domain.log.domain
 import com.dsm.fanflow.domain.log.domain.group.Group
 import com.dsm.fanflow.domain.user.domain.User
+import com.dsm.fanflow.global.BaseIdEntity
 import java.util.UUID
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
-import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
-
 @Entity
 class Log(
-    @Id
-    val id: UUID,
+    override val id: Long , // 부모 클래스에서 상속받은 id 필드
 
     var title: String,
     var content: String,
+    @Column(name = "log_group")
     @Enumerated(EnumType.STRING)
     var group: Group,
-    private var like: Int = 0,
+    private var likeCount: Int = 0,
     var image: String? = null,
 
     @ManyToOne
-    @JoinColumn(name = "user_accountId")
+    @JoinColumn(name = "user_id")
     val user: User
-) {
+): BaseIdEntity() {
+
+    // 생성자에서 id를 받지 않도록 수정
+
     fun addLike() {
-        like+=1
+        likeCount += 1
     }
 }
--- src/main/kotlin/com/dsm/fanflow/domain/log/domain/repository/LogRepository.kt
+++ src/main/kotlin/com/dsm/fanflow/domain/log/domain/repository/LogRepository.kt
@@ -7,8 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface LogRepository: JpaRepository<Log, UUID> {
+interface LogRepository: JpaRepository<Log, Long> {
     fun findLogsByUser(userId: UUID): List<Log>?
-
     fun findLogsByGroup(group: Group): List<Log>?
+    fun findLogById(id: Long): Log
+    fun existsLogById(id: Long): Boolean
 }
