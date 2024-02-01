de1ecd7944c888e1953af0d4c67abee55ef38168
soohyeon <meltapplee@gmail.com>
2024-02-01 20:48:49 +0900

    modify :: errorcode

--- src/main/kotlin/com/dsm/fanflow/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/com/dsm/fanflow/global/error/exception/ErrorCode.kt
@@ -5,9 +5,13 @@ enum class ErrorCode(
     val message: String
 ) {
     //user
+    TOKEN_UNAUTHORIZED(401, "Token Unauthorized"),
     PASSWORD_MISMATCH(403, "Password Mismatch"),
     USER_NOT_FOUND(404, "User Not Found"),
     ACCOUNT_ID_ALREADY_EXIST(409, "AccountId Already Exist"),
+
+    //log
+    LOG_NOT_EXIST(404, "Log Not Exist"),
 
     //server
     INTERNAL_SERVER_ERROR(500, "Internal Server Error")
