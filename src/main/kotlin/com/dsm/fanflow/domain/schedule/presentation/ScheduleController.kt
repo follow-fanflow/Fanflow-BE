51d371b10aee44f38db128156e439afeb232c18c
soohyeon <meltapplee@gmail.com>
2024-02-02 22:23:43 +0900

    add :: controller

---
+++ src/main/kotlin/com/dsm/fanflow/domain/schedule/presentation/ScheduleController.kt
@@ -0,0 +1,9 @@
+package com.dsm.fanflow.domain.schedule.presentation
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/schedule")
+@RestController
+class ScheduleController {
+}
