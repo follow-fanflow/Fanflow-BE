8c44ccfda58c68e7a38e7546f4c9e01ced093ad6
soohyeon <meltapplee@gmail.com>
2024-02-10 01:27:23 +0900

    delete :: gitkeep

--- src/main/kotlin/com/dsm/fanflow/domain/place/presentation/dto/PlaceController.kt
+++
@@ -1,9 +0,0 @@
-package com.dsm.fanflow.domain.place.presentation.dto
-
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-@RequestMapping("/place")
-@RestController
-class PlaceController {
-}
