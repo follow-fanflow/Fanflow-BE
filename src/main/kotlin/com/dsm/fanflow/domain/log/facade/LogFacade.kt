a8e5bafbe11a0b24560bbf333491da8a5b3bffbb
soohyeon <meltapplee@gmail.com>
2024-02-09 20:04:49 +0900

    add :: myPage api

--- src/main/kotlin/com/dsm/fanflow/domain/log/facade/LogFacade.kt
+++ src/main/kotlin/com/dsm/fanflow/domain/log/facade/LogFacade.kt
@@ -2,6 +2,7 @@ package com.dsm.fanflow.domain.log.facade
 
 import com.dsm.fanflow.domain.log.domain.Log
 import com.dsm.fanflow.domain.log.domain.repository.LogRepository
+import com.dsm.fanflow.domain.user.domain.User
 import com.dsm.fanflow.global.domain.enum.Group
 import org.springframework.stereotype.Component
 
@@ -9,10 +10,6 @@ import org.springframework.stereotype.Component
 class LogFacade(
     private val logRepository: LogRepository
 ) {
-
-    fun getLogsByUser(userId: String): List<Log>? {
-        return logRepository.findLogsByUser(userId)
-    }
 
     fun getLogsByGroupAndApprove(group: Group): List<Log>? {
         return logRepository.findLogsByGroupAndAndApprove(group, true)
@@ -28,6 +25,10 @@ class LogFacade(
 
     fun getApprovedLogs(): List<Log>? {
         return logRepository.findLogsByApprove(true)
+    }
+
+    fun getLogsByUser(user: User): List<Log>? {
+        return logRepository.findLogsByUser(user)
     }
 
 }
