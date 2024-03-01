def89ddb150be88202b4dd9ace20339008a608fc
soohyeon <meltapplee@gmail.com>
2024-03-01 21:42:03 +0900

    modify :: 뭐야 왜 발견못함?

--- src/main/kotlin/com/dsm/fanflow/domain/schedule/service/ListService.kt
+++ src/main/kotlin/com/dsm/fanflow/domain/schedule/service/ListService.kt
@@ -1,15 +1,15 @@
 package com.dsm.fanflow.domain.schedule.service
 
-import com.dsm.fanflow.domain.schedule.presentation.dto.response.EnumLitResponse
+import com.dsm.fanflow.domain.schedule.presentation.dto.response.EnumListResponse
 import com.dsm.fanflow.global.domain.enum.Member
 import org.springframework.stereotype.Service
 
 @Service
 class ListService {
-    fun getAllMembers(): EnumLitResponse {
+    fun getAllMembers(): EnumListResponse {
         val allMembers = Member.values().map { member ->
-            EnumLitResponse.MemberResponse(member)
+            EnumListResponse.MemberResponse(member)
         }
-        return EnumLitResponse(allMembers)
+        return EnumListResponse(allMembers)
     }
 }
